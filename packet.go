be670943ab7f5a484f9f30f197c253b30dc77667
MacDaih <gbrldavidson@gmail.com>
2025-05-03 16:11:49 +0200

    fix read properties

--- packet.go
+++ packet.go
@@ -64,7 +64,7 @@ func validateType(t byte) (packetType, error) {
 
 func newPacket(buff []byte) (*packet, error) {
 	cmd := buff[0] & 0xf0
-    flags := buf[0] & 0x0f
+    flags := buff[0] & 0x0f
 	pt, err := validateType(cmd)
 	if err != nil {
 		return nil, err
